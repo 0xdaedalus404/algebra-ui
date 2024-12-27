0a73c661ebcdbde7dc9c0bc872ea8c8aef7b6cb6
lilchizh <xlilchizhx@gmail.com>
2024-12-27 12:14:10 +0300

    add warning

--- src/components/common/Layout/index.tsx
+++ src/components/common/Layout/index.tsx
@@ -9,6 +9,7 @@ const Layout = ({ children }: LayoutProps) => {
     return (
         <div className="flex flex-col w-full h-full">
             <Header />
+            <div className='mt-12 p-2 bg-red-200 border border-2 font-bold border-red-700 text-red-500 rounded-lg'>💀 WARNING! This product is in the Beta stage. Use at your own risk & DYOR. 💀</div>
             <main className="h-full">{children}</main>
             <Toaster />
         </div>
