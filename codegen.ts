f3a281a3249d41ca4870e9ac9b907a5ad89a30f7
Kirill S <skdamn@outlook.com>
2025-04-04 14:19:13 +0300

    redeploy subgraphs

--- codegen.ts
+++ codegen.ts
@@ -3,10 +3,10 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 const config: CodegenConfig = {
     overwrite: true,
     schema: [
-        "https://api.studio.thegraph.com/query/50593/clamm-analytics/version/latest ",
-        "https://api.studio.thegraph.com/query/50593/clamm-blocks/version/latest",
-        "https://api.studio.thegraph.com/query/50593/clamm-farms/version/latest",
-        "https://api.studio.thegraph.com/query/50593/clamm-limits/version/latest",
+        "https://gateway.thegraph.com/api/4d7b59e4fd14365ae609945af85f3938/subgraphs/id/5pwQHNUqE7GFeG7C32m2HM3vhXvoQpet4HAinmHFmxW5",
+        "https://gateway.thegraph.com/api/4d7b59e4fd14365ae609945af85f3938/subgraphs/id/5ASqXjxabMNbiqXML7YBMDR7QVRgU6oFe5R8sYi4Uwtn",
+        "https://gateway.thegraph.com/api/4d7b59e4fd14365ae609945af85f3938/subgraphs/id/2whZiGiG7bRKgZcgvetHFrmJ59Z82SngfgorUcoqksJm",
+        "https://gateway.thegraph.com/api/4d7b59e4fd14365ae609945af85f3938/subgraphs/id/3dGcgzyDKZHZK8ykeyguecQwamriG6nQpDWCs8j9PiAS",
     ],
     documents: "src/graphql/queries/!(*.d).{ts,tsx}",
     generates: {
